f81bfb9acc6be1ada0a91a412ab986e96c02d685
Vinicius Dias <vinedias@gmail.com>
2020-06-21 11:42:17 -0300

    Add coco-ssd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as cocoSSD from '@tensorflow-models/coco-ssd';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'coco-ssd-bug';
+export class AppComponent implements OnInit {
+  @ViewChild('img', { static: true }) imagem: ElementRef<HTMLImageElement>;
+
+  async ngOnInit() {
+    const model = await cocoSSD.load();
+    const result = await model.detect(this.imagem.nativeElement);
+    console.log(result);
+  }
+
 }
